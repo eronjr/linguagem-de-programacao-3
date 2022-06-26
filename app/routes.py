f3194ca0a2bcfbde37e5ae3f79b6929e1a657213
J0a0Paul0Jp <jp.sousa.sds@gmail.com>
2022-06-26 20:44:06 -0300

    update

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,13 @@
-from requests import request
 from app import app
 from flask import render_template
 import os
-import requests
 
 @app.route("/login")
 def login():
-	print(os.getcwd())
 	return render_template("login.html") 
 
 
 @app.route("/index")
 @app.route("/")
 def index():
-	print(os.getcwd())
 	return render_template("index.html", content="Testing") 
