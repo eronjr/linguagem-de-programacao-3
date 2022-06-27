f0a5ecf6425e51408755fc7b72e5a4f0a9c1f13f
J0a0Paul0Jp <jp.sousa.sds@gmail.com>
2022-06-27 11:27:14 -0300

    update

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,5 +1,5 @@
 function updateGraphic() {
-    document.querySelector("#plot-graphic").src = "http://127.0.0.1:5000/api/indicadores_anual?anos=2015&anos=2016&anos=2017&categoria=categoria2";
+    document.querySelector("#plot-graphic").src = "https://indicadoresrs.herokuapp.com/api/indicadores_anual?anos=2015&anos=2016&anos=2017&categoria=categoria2";
 }
 
 // setInterval(function() {
