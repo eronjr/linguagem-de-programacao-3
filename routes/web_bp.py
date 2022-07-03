dc2b523b0a970bce4abca3bf329abf177d75666e
J0a0Paul0Jp <jp.sousa.sds@gmail.com>
2022-07-03 15:01:45 -0300

    update: atualizando para modelo mvc

---
+++ routes/web_bp.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from controllers.PagesController import index
+from controllers.PagesController import login
+
+web_bp = Blueprint('web_app', __name__)
+web_bp.route('/', methods=['GET'])(index)
+web_bp.route('/index', methods=['GET'])(index)
+web_bp.route('/login', methods=['GET','POST'])(login)
+
